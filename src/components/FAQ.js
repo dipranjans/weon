df6a15886720e0e8684ca31d0d9f76e2f11399c2
Diptiranjan Sukla <sukla.dipti@gmail.com>
2020-04-02 14:09:01 +0530

    Updates

--- src/components/FAQ.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class FAQ extends Component {
-  render() {
-    return <h5>FAQ</h5>;
-  }
-}
-
-export default FAQ;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "materialize-css";
-import "materialize-css/dist/css/materialize.min.css";
-import "./index.css";
-import App from "./App";
-
-ReactDom.render(<App />, document.getElementById("root"));
